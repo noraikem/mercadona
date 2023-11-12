89c1ce1fb1c4170cddb005e602c9166b51c6a6cf
noraikem <nora.badaoui.95@gmail.com>
2023-11-12 18:25:03 +0100

    delete all

--- backend/fastapi/routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-from . import models, schemas, database
-
-router = APIRouter()
-
-@router.get("/products/{product_id}")
-def read_product(product_id: int):
-    return {"product_id": product_id, "product_name": "Example Product"}
